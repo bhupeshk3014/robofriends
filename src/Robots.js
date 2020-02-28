5744ce2ed08050d73c533d425e6e6bf1f5671943
Bhupesh Kumar <bhupeshkumar003@gmail.com>
2020-02-28 11:33:53 +0530

    final changes

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const Robots = [
+  {
+    id: 1,
+    name: "Bhupesh Kumar",
+    username: "Bhupesh",
+    email: "bhupesh@kumar.biz"
+  },
+  {
+    id: 2,
+    name: "Khushboo Howell",
+    username: "Khushi",
+    email: "khushboo@melissa.tv"
+  },
+  {
+    id: 3,
+    name: "Neelima Bauch",
+    username: "Neelu",
+    email: "neelima@yesenia.net"
+  },
+  {
+    id: 4,
+    name: "Archit Lebsack",
+    username: "Tappu",
+    email: "archit@tmkoc.org"
+  },
+  {
+    id: 5,
+    name: "Amit Dietrich",
+    username: "Chote",
+    email: "chote@anime.ca"
+  },
+  {
+    id: 6,
+    name: "Ravisha Prakash",
+    username: "Rishu",
+    email: "tintin@jasper.info"
+  },
+  {
+    id: 7,
+    name: "Ajay Prasad Mahato",
+    username: "APM",
+    email: "apm@vlsi.biz"
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me"
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io"
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz"
+  }
+];
