62481c706192ffc2392b4822ee5359410f6e4f91
Bhupesh Kumar <bhupeshkumar003@gmail.com>
2020-03-02 19:11:01 +0530

    Updates

--- precache-manifest.6ddc22789033ba61f141432e1c2e841e.js
+++ precache-manifest.6ddc22789033ba61f141432e1c2e841e.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d3415cb5957fd06e0ac0697c34bd9b57",
+    "revision": "c81994180419990fd731b7565d6e8d09",
     "url": "/robofriends/index.html"
   },
   {
-    "revision": "10b3190f9b0b7b614b87",
+    "revision": "61fe6f8205a8e195e0e5",
     "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "c2e990f2ac28428d4f19",
+    "revision": "46c963d90e49c35e9f1d",
     "url": "/robofriends/static/css/main.0eb426c1.chunk.css"
   },
   {
-    "revision": "10b3190f9b0b7b614b87",
-    "url": "/robofriends/static/js/2.4b6b2329.chunk.js"
+    "revision": "61fe6f8205a8e195e0e5",
+    "url": "/robofriends/static/js/2.1f48fb4e.chunk.js"
   },
   {
-    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
-    "url": "/robofriends/static/js/2.4b6b2329.chunk.js.LICENSE.txt"
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/robofriends/static/js/2.1f48fb4e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c2e990f2ac28428d4f19",
-    "url": "/robofriends/static/js/main.0bcf2e48.chunk.js"
+    "revision": "46c963d90e49c35e9f1d",
+    "url": "/robofriends/static/js/main.be6f23c5.chunk.js"
   },
   {
     "revision": "d6ceab57e5523008bfa1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.541f6850e0ff8d4f54b197c627465077.js"
+  "/robofriends/precache-manifest.6ddc22789033ba61f141432e1c2e841e.js"
 );
 
 self.addEventListener('message', (event) => {
